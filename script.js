a9ef98a5da8525d6e593846a750318bf87bd315e
tod619 <trevorodwyer619@hotmail.com>
2023-02-13 20:58:10 +0000

    Use setInterval to update countdown every second in script.js file

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const countdown = document.querySelector('.countdown')
 
-const deadline = new Date(2023,1,25,12,00,00)
+const interval = setInterval(() => {
+    const deadline = new Date(2023,1,25,12,00,00)
 
-const current = new Date()
+    const current = new Date()
 
-// Create days, hours, minutes, seconds using the date object
-const diff = deadline - current
-const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
-const minutes = Math.floor((diff / 1000 * 60) % 60)
-const seconds = Math.floor((diff / 1000) % 60)
+    // Create days, hours, minutes, seconds using the date object
+    const diff = deadline - current
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((diff / 1000 * 60) % 60)
+    const seconds = Math.floor((diff / 1000) % 60)
 
-// Insert days, hours, minutes, seconds into the DOM
-countdown.innerHTML = `
-    <div>${days}</div>
-    <div>${hours}</div>
-    <div>${minutes}</div>
-    <div>${seconds}</div>
-`
+    // Insert days, hours, minutes, seconds into the DOM
+    countdown.innerHTML = `
+        <div>${days}</div>
+        <div>${hours}</div>
+        <div>${minutes}</div>
+        <div>${seconds}</div>
+    `
+}, 1000)
+
+
 
 
