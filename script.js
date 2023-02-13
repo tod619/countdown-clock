ad3cd48ee7d17ea00024993ef636db056b9e5237
tod619 <trevorodwyer619@hotmail.com>
2023-02-13 21:51:42 +0000

    Add leading 0 to countdown elements in script.js file

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const interval = setInterval(() => {
 
     // Create days, hours, minutes, seconds using the date object
     const diff = deadline - current
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
-    const minutes = Math.floor((diff / 1000 * 60) % 60)
-    const seconds = Math.floor((diff / 1000) % 60)
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + ""
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + ""
+    const minutes = Math.floor((diff / 1000 * 60) % 60) + ""
+    const seconds = Math.floor((diff / 1000) % 60) + ""
 
     // Insert days, hours, minutes, seconds into the DOM
     countdown.innerHTML = `
-        <div data-content="Days">${days}</div>
-        <div data-content="Hours">${hours}</div>
-        <div data-content="Minutes">${minutes}</div>
-        <div data-content="Seconds">${seconds}</div>
+        <div data-content="Days">${days.length === 1 ? `0${days}`: days}</div>
+        <div data-content="Hours">${hours.length === 1 ? `0${hours}`: hours}</div>
+        <div data-content="Minutes">${minutes.length === 1 ? `0${minutes}`: minutes}</div>
+        <div data-content="Seconds">${seconds.length === 1 ? `0${seconds}`: seconds}</div>
     `
 }, 1000)
 
